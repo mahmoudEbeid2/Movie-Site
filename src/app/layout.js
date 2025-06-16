a7a9a8b27ee5a476a058b56e205da49dcc99e313
AbdelmonemMarei <abdomarey006@gmail.com>
2025-06-16 16:10:25 +0300

    Initialize Next Movie App Project,Organize Folder Structure,add required packages

---
+++ src/app/layout.js
@@ -0,0 +1,34 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "@/components/Navbar";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Movie App",
+  description: "This is a movie app built with Next.js and powered by The Movie Database (TMDB) API.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
