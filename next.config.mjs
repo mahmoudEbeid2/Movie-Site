3792f02e1896296eb3c7876b90ef43872588e544
AbdelmonemMarei <abdomarey006@gmail.com>
2025-06-16 16:21:21 +0300

    edit next config file to use remote images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['image.tmdb.org'], // Allow images from TMDB
+      remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'image.tmdb.org',
+            port: '',
+            pathname: '/t/p/**',
+          },
+        ],
+      },
+  } 
+export default nextConfig
