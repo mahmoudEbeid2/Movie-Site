f4188b9ffcf16bef3f4c75f7f38f562eacbfdfac
mahmoudEbeid2 <mahmoud.ebeid2002@gmail.com>
2025-06-19 01:35:50 +0300

    Add loading spinner and not-found page

---
+++ src/app/loading.js
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import Spinner from "react-bootstrap/Spinner";
+
+export default function Loading() {
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center">
+        <h1>Loading .......</h1>
+        <Spinner variant="warning" animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import "@fortawesome/fontawesome-svg-core/styles.css"; 
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
 import { getNowPlayingMovies } from "@/app/api/themoviedbApi";
 import MovieContainer from "@/components/MovieContainer";
-import SearchForm from '@/components/SearchForm';
+import SearchForm from "@/components/SearchForm";
 
 export default async function Home() {
   const data = await getNowPlayingMovies();
@@ -15,16 +15,16 @@ export default async function Home() {
       <main className="flex flex-col gap-8 items-center">
         <div className="bg-[#F3F1F1] w-full p-4 ">
           <h2 className="text-3xl font-bold">Welcome to our movie app</h2>
-          <p className="text-sm font-normal my-4">Millions of movies, TV shows and people to discover. Explore now.</p>
-          <SearchForm/>
+          <p className="text-sm font-normal my-4">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </p>
+          <SearchForm />
         </div>
-        <h3 className="text-lg m-0 w-full font-bold text-start">
-          Now Playing
-        </h3>
-        
-        <MovieContainer 
-          initialMovies={data.results} 
-          initialTotalPages={data.total_pages} 
+        <h3 className="text-lg m-0 w-full font-bold text-start">Now Playing</h3>
+
+        <MovieContainer
+          initialMovies={data.results}
+          initialTotalPages={data.total_pages}
         />
       </main>
     </div>
