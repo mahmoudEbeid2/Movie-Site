1e54cb45d5d7aa1ddaad11151c1985e44bee9877
mahmoudEbeid2 <mahmoud.ebeid2002@gmail.com>
2025-06-18 04:10:34 +0300

    feat: add Favorites API (GET, POST, DELETE)

---
+++ src/app/api/favorites/route.js
@@ -0,0 +1,43 @@
+import Favorite from "../../../lib/models/Favorite";
+import { connectDB } from "../../../lib/db";
+import { NextResponse } from "next/server";
+
+// ✅ GET
+export async function GET() {
+  await connectDB();
+  const favorites = await Favorite.find({});
+  return NextResponse.json(favorites);
+}
+
+// ✅ POST:
+export async function POST(request) {
+  await connectDB();
+  const body = await request.json();
+
+  const exists = await Favorite.findOne({ id: body.id });
+  if (exists) {
+    return NextResponse.json(
+      { message: "Favorite already exists" },
+      { status: 400 }
+    );
+  }
+
+  const favorite = await Favorite.create(body);
+  return NextResponse.json(favorite);
+}
+
+// DELETE:
+export async function DELETE(request) {
+  await connectDB();
+  const { id } = await request.json();
+
+  const deleted = await Favorite.findOneAndDelete({ id });
+  if (!deleted) {
+    return NextResponse.json(
+      { message: "Favorite not found" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({ message: `Favorite ${id} deleted` });
+}
