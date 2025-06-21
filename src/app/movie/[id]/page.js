dc2f2d53054f9381ca0849a04419eeb67b1c066e
mohameddahroug <mhmeddahroug@gmail.com>
2025-06-21 18:40:54 +0300

    add movie details page

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,24 +1,70 @@
 import React from "react";
-import Image from "next/image";
 import { getMovieDetailsByID } from "@/app/api/themoviedbApi";
+import styles from "./movieDetails.module.css";
+import MovieDetails from "./movieDetails"; 
+import { useLanguage } from "@/app/contexts/LanguageContext";
+import MovieCard from "@/components/MovieCard/MovieCard";
 
 async function MovieDetailsPage({ params }) {
-  const { id } = params;
+  const { id } = await params;
+
   const movie = await getMovieDetailsByID(id);
   console.log(movie);
 
-  // destructure of object to use
-  // const { title, overview, genres, vote_average, credits, videos, recommendations,similar,production_companies, poster_path, budget, release_date, runtime ,revenue  } = await getMovieDetailsByID(id);
+
+  const {
+    title,
+    overview,
+    genres,
+    vote_average,
+    credits,
+    videos,
+    recommendations,
+    similar,
+    production_companies,
+    poster_path,
+    budget,
+    release_date,
+    runtime,
+    revenue,
+    vote_count,
+    original_language,
+   
+  } = movie;
 
   return (
-    <div>
-      {/* <Image
-              src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-              alt={title}
-              className="w-full max-h-[500px] object-contain rounded-lg"
-              width={300}
-              height={500}
-            /> */}
+ 
+
+    <div className="container  py-5">
+      <h2 className="fw-600 mb-4">deatiles List</h2>
+      <div className={styles.grid}>
+        <MovieDetails
+          key={id}
+          id={id}
+          title={title}
+          poster={`https://image.tmdb.org/t/p/w500${poster_path}`}
+          date={release_date}
+          rating={Math.round(vote_average / 2)}
+          voteCount={vote_average}
+          description={overview}
+          genres={genres}
+          production_companies={production_companies}
+          runtime={runtime}
+          original_language={original_language}
+          vote_count={vote_count}
+          release_date={release_date}
+
+        />
+      </div>
+      <h2 className="fw-600 mb-4">Recommendations</h2>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 mb-8">
+       
+        {recommendations.results.splice(0, 12).map((movie) => (
+          <div key={movie.id} className="col d-flex justify-content-center">
+            <MovieCard movie={movie} useFav={false} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
